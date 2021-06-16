64548a18327b2d1ac143cd0f9c6fe7df76b5222e
fabien-ors <fabien.ors@mines-paristech.fr>
2021-06-16 10:14:53 +0200

    Convert setup.cfg to setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-fabien-ors",
+    version="0.0.5",
+    author="Fabien Ors",
+    author_email="fabien.ors@mines-paristech.fr",
+    description="A small example package from official tutorial",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fabien-ors/example-pkg",
+    project_urls={
+        "Bug Tracker": "https://github.com/fabien-ors/example-pkg/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Geosciences :: Geostatistics :: Machine Learning",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
+
